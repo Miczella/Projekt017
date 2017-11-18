c39820e01ca4fd219912af834eac6aac0565648a
Miczella <j.justyna12@o2.pl>
2017-11-18 14:25:48 +0100

    Dodałam lubie czekolade

--- app/src/main/java/com/example/uczen/juicetea/MainActivity1.java
+++ app/src/main/java/com/example/uczen/juicetea/MainActivity1.java
@@ -1,13 +1,60 @@
 package com.example.uczen.juicetea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity1 extends AppCompatActivity {
+
+Button p1,p2,p3,p4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+        p1 = (Button) findViewById(R.id.button3);
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene1.class);
+                startActivity(intent);
+
+            }
+        });
+
+        p2 = (Button) findViewById(R.id.button4);
+
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene2.class);
+                startActivity(intent);
+
+            }
+              });
+        p3 = (Button) findViewById(R.id.button1);
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene3.class);
+                startActivity(intent);
+
+            }
+        });
+        p4 = (Button) findViewById(R.id.button2);
+
+        p4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Scene4.class);
+                startActivity(intent);
+
+            }
+    });
     }
 }
---
+++ app/src/main/java/com/example/uczen/juicetea/Scene4.java
@@ -0,0 +1,17 @@
+package com.example.uczen.juicetea;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by uczen on 2017-11-18.
+ */
+
+public class Scene4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scene4);
+    }
+}
